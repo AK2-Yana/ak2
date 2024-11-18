d04605fb7a5a6f5b4a4826c22f879f143bea0a59
“akki-to” <yanach1n919@gmail.com>
2024-11-18 20:39:00 +0900

    first commit

---
+++ src/main/java/ak2/services/ak2/Ak2Application.java
@@ -0,0 +1,13 @@
+package ak2.services.ak2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ak2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ak2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ak2/services/ak2/Ak2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ak2.services.ak2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ak2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
